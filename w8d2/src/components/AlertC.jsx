394e83c896536d59f260bbaf02d6a9863dbe6a0b
MicheleFamoso <michelefamoso@gmail.com>
2025-04-02 09:04:17 +0200

    .

--- w8d2/src/components/AlertC.jsx
+++ w8d2/src/components/AlertC.jsx
@@ -3,8 +3,8 @@ import Alert from "react-bootstrap/Alert";
 function AlertC() {
   return (
     <Container className="mt-3">
-      <Alert variant={"danger"} className=" text-center fs-2 font-monospace">
-        Racconti di legami,scelte e cambiamenti .
+      <Alert variant={"info"} className=" text-center fs-2 font-monospace">
+        Eventi, personaggi e storie che hanno segnato il mondo.
       </Alert>
     </Container>
   );
--- w8d2/src/components/FooterCustom.jsx
+++ w8d2/src/components/FooterCustom.jsx
@@ -1,12 +1,12 @@
 function FooterCustom() {
   return (
-    <div className=" w-50  d-flex  justify-content-center  bg-body-tertiary">
+    <div className=" w-50  d-flex  justify-content-center  align-items-center">
       <img
         src="https://instituteoftechnology.epicode.com/wp-content/uploads/2024/02/Institute-of-Technology.png"
         className=" w-25 "
         alt="logo"
       />{" "}
-      <h5 className="mt-2">©</h5>
+      <h2>•{new Date().getFullYear()}</h2>
     </div>
   );
 }
