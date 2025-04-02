cba23d76d29449a7f5835a3869e1547776757f1c
MicheleFamoso <michelefamoso@gmail.com>
2025-04-02 18:05:07 +0200

    Update BookList.jsx

--- w8d2/src/components/BookList.jsx
+++ w8d2/src/components/BookList.jsx
@@ -4,30 +4,48 @@ import SingleBook from "./SingleBook";
 import { Component } from "react";
 
 class BookList extends Component {
-    render(
-  return (
-    <>
-      <Container>
-        <Row>
-          <Form className="d-flex mt-3">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-          {props.book.map((sbook) => {
-            return (
-              <Col xs={12} md={4} lg={3} key={sbook.asin} className=" g-3">
-                <SingleBook img={sbook.img} title={sbook.title} />
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
-    </>
-  );)
+  state = {
+    search: "",
+  };
+
+  render() {
+    return (
+      <>
+        <Container>
+          <Row>
+            <Form className="d-flex mt-3">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                value={this.state.search}
+                onChange={(e) => {
+                  this.setState({ search: e.target.value });
+                }}
+              />
+            </Form>
+          </Row>
+          <Row>
+            {this.props.book
+              .filter((libro) => {
+                if (libro.title.includes(this.state.search)) {
+                  return true;
+                } else {
+                  return false;
+                }
+              })
+              .map((sbook) => {
+                return (
+                  <Col xs={12} md={4} lg={3} key={sbook.asin} className=" g-3">
+                    <SingleBook img={sbook.img} title={sbook.title} />
+                  </Col>
+                );
+              })}
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 export default BookList;
