e23ceed8123a022088f7740adea54b1e658e9208
MicheleFamoso <michelefamoso@gmail.com>
2025-04-04 08:48:24 +0200

    Update AddComment.jsx

--- w8d2/src/AddComment.jsx
+++ w8d2/src/AddComment.jsx
@@ -1,3 +1,5 @@
+const Url = "https://striveschool-api.herokuapp.com/api/comments/";
+
 import { Component } from "react";
 import {
   Form,
@@ -6,21 +8,66 @@ import {
   ListGroup,
   ListGroupItem,
   FormControl,
+  Button,
 } from "react-bootstrap";
 
 class AddComment extends Component {
   state = {
-    comment: [],
+    review: {
+      comment: "",
+      rate: "2",
+      elementId: this.props.asin,
+    },
   };
+
+  invia = (e) => {
+    e.preventDefault();
+    fetch(Url, {
+      method: "POST",
+      body: JSON.stringify(this.state.review),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2VlN2ZjNjFkNDM2ZTAwMTVkYTI3MjAiLCJpYXQiOjE3NDM2ODM1MjYsImV4cCI6MTc0NDg5MzEyNn0.rxE_OUjsSq3JbBKF0_8uZjrZD-Tl6XhoPN5yFmV3ziI",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Inviato!!!");
+        } else {
+          throw new Error("Non Inviato!");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <ListGroup>
         <ListGroupItem>
-          <Form>
+          <Form onSubmit={this.invia}>
             <FormGroup>
               <FormLabel>Recensione</FormLabel>
-              <FormControl type="text" placeholder="Recensisci" />
+              <FormControl
+                type="text"
+                placeholder="Recensisci"
+                value={this.state.review.comment}
+                required
+                onChange={(e) => {
+                  this.setState({
+                    review: {
+                      ...this.state.review,
+                      comment: e.target.value,
+                    },
+                  });
+                }}
+              />
             </FormGroup>
+
+            <Button>Invia</Button>
           </Form>
         </ListGroupItem>
       </ListGroup>
